94522f9d6b18a4fe561edcb3d31bc993de53d943
Jesse B. Hannah <jesse@jbhannah.net>
2017-02-18 15:49:10 -0700

    Load scoreboard pack in scoreboard layout

--- app/javascript/packs/scoreboard.js
+++ app/javascript/packs/scoreboard.js
@@ -1,4 +1,4 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// Run this example by adding <%= javascript_pack_tag 'scoreboard' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
@@ -12,5 +12,5 @@ class Hello extends React.Component {
 }
 
 document.addEventListener("DOMContentLoaded", e => {
-  ReactDOM.render(<Hello name="React" />, document.body.appendChild(document.createElement('div')))
+  ReactDOM.render(<Hello name="React" />, document.getElementById('scoreboard'))
 })
