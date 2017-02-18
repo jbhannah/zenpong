63729e30ecd6d1ec3d483869c151efcba8e7a703
Jesse B. Hannah <jesse@jbhannah.net>
2017-02-18 15:58:57 -0700

    Extract scoreboard App component

--- app/javascript/packs/scoreboard.js
+++ app/javascript/packs/scoreboard.js
@@ -1,16 +1,1 @@
-// Run this example by adding <%= javascript_pack_tag 'scoreboard' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-class Hello extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}!</div>
-  }
-}
-
-document.addEventListener("DOMContentLoaded", e => {
-  ReactDOM.render(<Hello name="React" />, document.getElementById('scoreboard'))
-})
+require('../scoreboard')
